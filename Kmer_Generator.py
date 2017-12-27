0749846f67c9c9cba9579f3b95f57827d55bd01d
ritavityaz <margaryta.vityaz@gmail.com>
2017-12-26 23:55:47 -0800

    Add files via upload

---
+++ Kmer_Generator.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+
+
+# makes dict of strings, of size k
+
+def kmerGenerator(inputStrings, k):
+    numberOfStrings = len(inputStrings)
+    kmers = dict.fromkeys(np.arange(numberOfStrings))
+
+    for j, string in enumerate(inputStrings):
+        extendedString = string + string[:k-1]
+        kmers[j] = [extendedString[i:i + k] for i in range(0, len(extendedString) - k + 1)]
+    return kmers
+
+
+
+
+# kmerGenerator(["1234","2345","3456","4567"],2)
+
+
