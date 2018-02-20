5e56c767d19c139e61b05b3ae070852c2f8bf7ae
ritavityaz <margaryta.vityaz@gmail.com>
2018-02-20 14:07:36 -0800

    Fixed the bug

--- FINE_filtering.py
+++ FINE_filtering.py
@@ -4,30 +4,37 @@ def fine_filtering(templates, reads, k, f):
 
     number_of_templates = len(templates)
     length_of_read = len(reads[0])
-
     template_coverage = [0] * number_of_templates
-
 
     for ind, template in enumerate(templates):
 
-        extended_template = template + template[:k - 1]
-        template_length = len(extended_template)
-        all_kmer_templates = [extended_template[i:i + k] for i in range(0, template_length - k + 1)]
-        total_number_of_template_kmers = len(all_kmer_templates)
+        template_length = len(template)
 
-        kmer_templates = set(all_kmer_templates)
+        kmer_templates = list(set([template[i:i + k] for i in range(0, template_length - k + 1)]))
+
         number_of_kmers = len(kmer_templates)
 
         kmer_matches = [0]*number_of_kmers
 
         for read in reads:
-            kmer_reads = [read[i:i + k] for i in range(0, length_of_read - k + 1)]
-            read_kmer_matches = [i for i, item in enumerate(kmer_templates) if item in kmer_reads]
 
-            if len(read_kmer_matches) >= total_number_of_template_kmers - f:
+            kmer_reads = [read[i:i + k] for i in range(0, length_of_read - k + 1)]
+
+            read_kmer_matches = [kmer_templates.index(i) for i in kmer_reads if i in kmer_templates]
+
+            if len(read_kmer_matches) >= f:
                 for match in read_kmer_matches:
                     kmer_matches[match] += 1
 
         template_coverage[ind] = median(kmer_matches)
 
     return template_coverage
+
+
+#
+#
+#
+#         # print kmer_templates
+#
+# print( fine_filtering(["12345867"], ["12345", "23999", "56799"], 2, 0))
+#
