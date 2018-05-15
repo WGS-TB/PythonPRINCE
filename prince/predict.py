0ddd3860d21665b13f86f9e0f88e9d7f11b250f5
Dan Fornika <dfornika@gmail.com>
2018-05-15 16:25:51 -0700

    Removed tab characters from indentation

--- prince/predict.py
+++ prince/predict.py
@@ -5,20 +5,20 @@ import csv
 def get_X_and_Y(data,template):
     X,Y = [],[]
     for genome,templateName,tnum,cn,score in data:
-	if score == 0: continue
-	if template == int(tnum): 
-	    X.append(score)
-	    Y.append(cn)
+        if score == 0: continue
+        if template == int(tnum):
+            X.append(score)
+            Y.append(cn)
     return(X,Y)
 
 def get_equations(data):
     equations = []
     for t in range(24):
-	X,Y = get_X_and_Y(data,t)
-	X = np.array(X, dtype=np.float64)
-	Y = np.array(Y, dtype=np.float64)
-	slope, intercept, r_value, p_value, std_err = stats.linregress(X,Y)
-	equations.append((slope,intercept))
+        X,Y = get_X_and_Y(data,t)
+        X = np.array(X, dtype=np.float64)
+        Y = np.array(Y, dtype=np.float64)
+        slope, intercept, r_value, p_value, std_err = stats.linregress(X,Y)
+        equations.append((slope,intercept))
     return(equations)
 
 def get_copy_number(x,slope,intercept):
