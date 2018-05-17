54ce109d2c02121f60999fc1c94f65f99e5be165
Dan Fornika <dfornika@gmail.com>
2018-05-16 17:25:40 -0700

    Added some unit tests for predict.py

---
+++ prince/tests/test_predict.py
@@ -0,0 +1,27 @@
+import unittest
+
+from prince.predict import get_copy_number
+
+class GetCopyNumberTest(unittest.TestCase):
+    
+    def test_get_copy_number(self):
+        self.assertEqual(get_copy_number(0, 0, 0), 0)
+        self.assertEqual(get_copy_number(1, 0, 0), 0)
+        self.assertEqual(get_copy_number(0, 1, 0), 0)
+        self.assertEqual(get_copy_number(0, 0, 1), 1)
+        self.assertEqual(get_copy_number(0, 1, 1), 1)
+        self.assertEqual(get_copy_number(1, 0, 1), 1)
+        self.assertEqual(get_copy_number(1, 1, 0), 1)
+        self.assertEqual(get_copy_number(1, 1, 1), 2)
+        self.assertEqual(get_copy_number(-1, 0, 0), 0)
+        self.assertEqual(get_copy_number(0, -1, 0), 0)
+        self.assertEqual(get_copy_number(0, 0, -1), -1)
+        self.assertEqual(get_copy_number(0, -1, -1), -1)
+        self.assertEqual(get_copy_number(-1, 0, -1), -1)
+        self.assertEqual(get_copy_number(-1, -1, 0), 1)
+        self.assertEqual(get_copy_number(-1, -1, -1), 0)
+        self.assertEqual(get_copy_number(0, 1, -1), -1)
+        self.assertEqual(get_copy_number(0, -1, 1), 1)
+        self.assertEqual(get_copy_number(-1, 0, 1), 1)
+        self.assertEqual(get_copy_number(100, 100, 100), 10100)
+        self.assertEqual(get_copy_number(987, 896, 876), 885228)
