2b1fe44169792fcf38e6426ba6b954fd84b9d18b
Julius Booth <boothjulius@gmail.com>
2018-10-14 22:37:26 -0700

    using k=25 now

--- prince/coarse_filtering.py
+++ prince/coarse_filtering.py
@@ -2,47 +2,40 @@ from Bio.Seq import Seq
 
 def coarse_filtering(reads, k, template_kmers, quality_filter=20):
     recruitedReads = []
-    template_set = set([x for v in template_kmers.values() for x in v])
+    template_set = set([kmer for value in template_kmers.values() for kmer in value])
     skipped_reads = 0
     
-    for j,record in enumerate(reads):
-        sequence  = record.seq
+    for j, record in enumerate(reads):
+        sequence = record.seq
+        quality = record.letter_annotations["phred_quality"]
+
         if j == 0:
             read_length = len(sequence)
+
         #Generate k-splits
-        reads = [sequence[n:n+25] for n in range(0,read_length-25+1,25)]
-        
-        quality = record.letter_annotations["phred_quality"]
-        
+        reads = [sequence[n:n+25] for n in range(0, read_length-25+1, 25)]
+
         if j == 0:
-            lengthOfReads = len(reads[0])
-            leftoverPiece = lengthOfReads % k
-            split_length = lengthOfReads - leftoverPiece
             k_splits_per_read = len(reads)
         
-        for i,read in enumerate(reads):
+        for i, read in enumerate(reads):
             
             #Throw away low quality sections of the read and record the number of disposals.
             avg_qual = sum(quality[i*25:(i*25)+25])/25.0
             if avg_qual < quality_filter:
-                skipped_reads +=1
+                skipped_reads += 1
                 continue
             
             # Forward
             readSeq = str(read)
-            listOfSplits = [readSeq[i:i + k] for i in range(0, split_length, k)]
-            recruited = [r in template_set for r in listOfSplits]
-            
-            if True in recruited:
-                 recruitedReads.append(readSeq)
-            
+            if readSeq in template_set:
+                recruitedReads.append(readSeq)
+
             # Reverse
             reverseRead = str((read.reverse_complement()))
-            listOfSplits = [reverseRead[i:i + k] for i in range(0, split_length, k)]
-            recruited = [r in template_set for r in listOfSplits]
+            if reverseRead in template_set:
+                recruitedReads.append(reverseRead)
 
-            if True in recruited:
-                 recruitedReads.append(reverseRead)
     #Nucleotides seen takes into account number of reads, read length and skipped read sections
     nucleotides_seen = (j+1)*k_splits_per_read-skipped_reads
     return(nucleotides_seen, recruitedReads)
--- prince/fine_filtering.py
+++ prince/fine_filtering.py
@@ -1,6 +1,5 @@
 def fine_filtering(templates, reads, k):
     number_of_templates = len(templates)
-    length_of_read = len(reads[0])
     template_coverage = [0] * number_of_templates
 
     for ind, template in enumerate(templates):
@@ -9,9 +8,7 @@ def fine_filtering(templates, reads, k):
         template_kmers = set([extended_template[i:i + k] for i in range(0, template_length - k + 1)])
 
         for read in reads:
-            read_kmers = (read[i:i + k] for i in range(0, length_of_read - k + 1))
-            num_read_matches = sum([1 for kmer in read_kmers if kmer in template_kmers])
+            if read in template_kmers:
+                template_coverage[ind] += 17
 
-            if num_read_matches >= length_of_read-k+1:
-                template_coverage[ind] += num_read_matches
     return template_coverage
