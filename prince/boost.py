20033d425ae844f9c52dc9508bcdd6fffbee5a7f
john <johnathanwong4@gmail.com>
2018-11-02 14:13:06 -0700

    modified boost.py to use Johnathan's modified function of match_score

--- prince/boost.py
+++ prince/boost.py
@@ -4,10 +4,14 @@ def run_boosts(opts,templates,templateNames,templateKmers):
     with open(opts.boosting_file) as file:
         sample = file.readline().strip("\n")
         while sample:
-
-            sampleFileName = sample.split("/")[-1]
+            sampleFileName = sample.split("\t")[0].split("/")[-1] 
+            sample_forward = sample.split("\t")[0]
+            try:
+                sample_reverse = sample.split("\t")[1]
+            except:
+                sample_reverse = ""
             print("\nTraining with %s" % sampleFileName)
-            boostingMatchScore = compute_match_score(sample, templates, templateKmers, opts.k)
+            boostingMatchScore = compute_match_score(sample_forward, sample_reverse,templates, templateKmers, opts.k)
             with open(opts.boost_output, "a") as f:
                 for t_num, matchscore in enumerate(boostingMatchScore):
                     f.write(sampleFileName + "," + templateNames[t_num] + "," + str(t_num) + "," + str(opts.copynumber) + "," + str(
