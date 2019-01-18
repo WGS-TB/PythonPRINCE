b893a43b67a0d24c771e3def7543820d177c39d1
Julius Booth <jbooth@sabin.jgardy.bcgsc.ca>
2019-01-17 18:33:17 -0800

    fixed current tests

--- prince/tests/test_kmer_generator.py
+++ prince/tests/test_kmer_generator.py
@@ -6,4 +6,6 @@ class KmerGeneratorTest(unittest.TestCase):
     
     def test_kmer_generator(self):
         self.assertEqual(kmer_generator("", 0), {})
-        self.assertEqual(kmer_generator("A",1), {0: ['A']})
+        self.assertEqual(kmer_generator("A",1), {0: ['A', 'T']})
+	self.assertEqual(kmer_generator(["ATTCG","GGGG"],3), {0: ['ATT', 'TTC', 'TCG', 'CGA', 'GAT', 'ATC', 'TCG', 'CGA', 'GAA', 'AAT'], 1: ['GGG', 'GGG', 'GGG', 'GGG', 'CCC', 'CCC', 'CCC', 'CCC']})
+	self.assertEqual(kmer_generator(["ATTCG","GGGG"],3, extension=False), {0: ['ATT', 'TTC', 'TCG', 'CGA', 'GAA', 'AAT'], 1: ['GGG', 'GGG', 'CCC', 'CCC']})
